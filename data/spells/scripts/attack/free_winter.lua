154244ff8452b326571e1894c5e115e1a3c77c77
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-04-22 01:14:56 -0700

    Multicast free winter with varied area
    - Adds an array of combats to free_winter.lua to execute the spell multiple times and extend the length.
    - Adds and implements a new combatArea SCATTER3X3 to the spells library to allow for varied targets when recasting free winter.
    - Adds constant SPELL_REPEATS to free_winter.lua for easy configuration.

--- data/spells/scripts/attack/free_winter.lua
+++ data/spells/scripts/attack/free_winter.lua
@@ -1,8 +1,5 @@
-local combat = Combat()
-
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat:setArea(createCombatArea(AREA_CIRCLE3X3)) --can create custom in lib if needed
+local combats = {}
+local SPELL_REPEATS = 6 -- if > 6 add to SCATTER3x3
 
 function onGetFormulaValues(player, level, magicLevel)
 	local min = (level / 5) + (magicLevel * 5.5) + 25
@@ -10,15 +7,27 @@ function onGetFormulaValues(player, level, magicLevel)
 	return -min, -max
 end
 
-combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+for initializer = 1, SPELL_REPEATS do
+	-- create a sequence of combats with different areas
+	newCombat = Combat()
+	newCombat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+	newCombat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+	newCombat:setArea(createCombatArea(SCATTER3X3[initializer]))
+	function onGetFormulaValues(player, level, magicLevel)
+		local min = (level / 5) + (magicLevel * 5.5) + 25
+		local max = (level / 5) + (magicLevel * 11) + 50
+		return -min, -max
+	end
+	newCombat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+	combats[initializer] = newCombat
+end
 
 function onCastSpell(creature, variant)
-	-- we will recast the spell every second
-	-- for 3 seconds to match the longer video length
-	local repeats = 6
-	for counter = 1, repeats do
+	-- recast the spell several times
+	-- to increase the length and allow for new areas
+	for counter = 1, SPELL_REPEATS do
 		addEvent(function () 
-			combat:execute(creature, variant)
+			combats[counter]:execute(creature, variant)
 		end, counter * 500) -- 500 ms == 0.5s
 	end
 end
