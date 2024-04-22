61dc20f6727c509c372c419d17f9087f90fcb4b2
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-04-21 22:34:38 -0700

    Extend free winter duration

--- data/spells/scripts/attack/free_winter.lua
+++ data/spells/scripts/attack/free_winter.lua
@@ -1,7 +1,8 @@
 local combat = Combat()
+
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
-combat:setArea(createCombatArea(AREA_CIRCLE3X3)) --correct size, but visually misaligned. can edit in lib if needed
+combat:setArea(createCombatArea(AREA_CIRCLE3X3)) --can create custom in lib if needed
 
 function onGetFormulaValues(player, level, magicLevel)
 	local min = (level / 5) + (magicLevel * 5.5) + 25
@@ -12,5 +13,12 @@ end
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+	-- we will recast the spell every second
+	-- for 3 seconds to match the longer video length
+	local repeats = 6
+	for counter = 1, repeats do
+		addEvent(function () 
+			combat:execute(creature, variant)
+		end, counter * 500) -- 500 ms == 0.5s
+	end
 end
